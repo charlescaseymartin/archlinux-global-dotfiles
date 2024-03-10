8ffbd2041f74d16940bc18f270c237af5d0f3c04
charlescaseymartin <charlescaseymartin@gmail.com>
2024-03-10 23:26:04 +0200

    removed xdg_config_home update

--- install.sh
+++ install.sh
@@ -64,11 +64,11 @@ install_dotfiles() {
         env cp -rf "$dotfiles_repo_dir/${dots_home}" "$HOME/"
     done
 
-    # Install $XDG_CONFIG_HOME configs.
-    mkdir -p "$XDG_CONFIG_HOME"
+    # Install $HOME/.config configs.
+    mkdir -p "$HOME/.config"
     for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]}"
     do
-        env rm -rf "$XDG_CONFIG_HOME${dots_xdg_conf[*]//./}"
+        env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
         env cp -rf "$dotfiles_repo_dir/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
     done
 
@@ -93,8 +93,8 @@ uninstall_dotfiles() {
 
         for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
         do
-            env rm -rf "$XDG_CONFIG_HOME${dots_xdg_conf}"
-            env cp -rf "$backup_dir/.config/${dots_xdg_conf}" "$XDG_CONFIG_HOME" &> /dev/null
+            env rm -rf "$HOME/.config/${dots_xdg_conf}"
+            env cp -rf "$backup_dir/.config/${dots_xdg_conf}" "$HOME/.config" &> /dev/null
             env rm -rf "$backup_dir/.config/${dots_xdg_conf}"
         done
 
